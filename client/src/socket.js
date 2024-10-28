47af4960b82f89edc62129d6c9affb29758f32b5
oVTPo <phatvuongtan0607@gmail.com>
2024-10-28 15:10:49 +0700

    Cập nhật socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
 // Thay thế 'http://localhost:3001' bằng URL server trên Fly.io
-const socket = io("https://server-still-cloud-6652.fly.dev", {
+export const socket = io("https://server-still-cloud-6652.fly.dev", {
   transports: ['websocket'], // Sử dụng WebSocket để kết nối
 });
