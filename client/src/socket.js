d9a1bdd58a71e7f0f374c75e21eaab26fc3dc81a
oVTPo <phatvuongtan0607@gmail.com>
2024-10-28 15:15:16 +0700

    Cập nhật socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,16 @@
+// Giả sử đây là file socket.js hoặc trong một component nào đó
+
 import io from 'socket.io-client';
 
 // Thay thế 'http://localhost:3001' bằng URL server trên Fly.io
-export const socket = io("https://server-still-cloud-6652.fly.dev", {
+const socket = io("https://server-still-cloud-6652.fly.dev", {
   transports: ['websocket'], // Sử dụng WebSocket để kết nối
 });
+
+// Tiếp tục với các sự kiện Socket.IO mà bạn muốn lắng nghe hoặc phát
+socket.on('receiveOrder', (data) => {
+  console.log('Received order:', data);
+});
+
+// Xuất socket nếu cần thiết
+export default socket;
